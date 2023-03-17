77c7a5fa1465e15284dfc0e04cbf8453e67039c9
Tan Yi Jun <tanyijun6@gmail.com>
2023-03-17 16:12:47 +0800

    Create operation

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case 'add':
+            return (add(num1, num2));
+
+        case 'subtract':
+            return (subtract(num1, num2));
+
+        case 'multiply':
+            return (multiply(num1, num2));
+
+        case 'divide':
+            return (divide(num1, num2));
+    }
+}
