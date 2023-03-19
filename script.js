8c9eb26b80401c6c12c7cea842c2c513d98d0c47
Tan Yi Jun <tanyijun6@gmail.com>
2023-03-20 00:24:52 +0800

    Add working operand

--- script.js
+++ script.js
@@ -30,14 +30,48 @@ function operate(operator, num1, num2) {
     }
 }
 
+function getOperand(operand) {
+    switch(operand) {
+        case '+':
+            return 'add';
+
+        case '-':
+            return 'subtract';
+
+        case '*':
+            return 'multiply';
+
+        case '/':
+            return 'divide';
+    }
+}
+
 function populateDisplay(e) {
-    const currentValue = document.querySelector('#display-current');
-    if (e.target.className === 'number') {
-        if (currentValue.innerText === '0') {
-            currentValue.innerText = e.target.innerText;
+    const selection = e.target.className;
+    const current = document.querySelector('#current-value');
+    const operation = document.querySelector("#operation");
+    const currentValue = +current.innerText;
+
+    if (selection === 'number') {
+        if (currentValue === 0 || current.classList.contains('saved')) {
+            current.innerText = e.target.innerText;
+            current.classList.remove('saved');
         } else {
-            currentValue.innerText += e.target.innerText;
+            current.innerText += e.target.innerText;
         }
+    } else if (selection === 'enter') {
+
+    } else if (selection === 'operand') {
+        if(operation.innerText) {
+            const lastValue = +operation.innerText.slice(0, -2);
+            const operand = getOperand(operation.innerText.slice(-1));
+            const result = operate(operand, lastValue, currentValue);
+            operation.innerText = `${result} ${e.target.innerText}`;
+        } else {
+            operation.innerText = `${+current.innerText} ${e.target.innerText}`;
+        }
+
+        current.classList.add('saved');
     }
 }
 
