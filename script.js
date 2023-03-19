29fbd92b3aa360dd1230b7ef87ada065b3c73ad3
Tan Yi Jun <tanyijun6@gmail.com>
2023-03-20 01:44:42 +0800

    Get enter button working

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function operate(operator, num1, num2) {
     }
 }
 
-function getOperand(operand) {
+function getOperandText(operand) {
     switch(operand) {
         case '+':
             return 'add';
@@ -46,8 +46,31 @@ function getOperand(operand) {
     }
 }
 
+function getOperandSymbol(operand) {
+    switch(operand) {
+        case 'add':
+            return '+';
+
+        case 'subtract':
+            return '-';
+
+        case 'multiply':
+            return '*';
+
+        case 'divide':
+            return '/';
+    }
+}
+
+function cleanUp() {
+    const current = document.querySelector('#current-value');
+    const operation = document.querySelector("#operation");
+    current.innerText = 0;
+    operation.innerText = '';
+}
+
 function populateDisplay(e) {
-    const selection = e.target.className;
+    const selection = e.target.dataset.type;
     const current = document.querySelector('#current-value');
     const operation = document.querySelector("#operation");
     const currentValue = +current.innerText;
@@ -60,15 +83,25 @@ function populateDisplay(e) {
             current.innerText += e.target.innerText;
         }
     } else if (selection === 'enter') {
-
-    } else if (selection === 'operand') {
-        if(operation.innerText) {
+        if (current.classList.contains('saved')) {
+            return;
+        }
+        if (operation.innerText) {
             const lastValue = +operation.innerText.slice(0, -2);
-            const operand = getOperand(operation.innerText.slice(-1));
+            const operand = getOperandText(operation.innerText.slice(-1));
+            const result = operate(operand, lastValue, currentValue);
+            operation.innerText = `${lastValue} ${getOperandSymbol(operand)} ${currentValue} =`;
+            current.innerText = result;
+            current.classList.add('saved');
+        }
+    } else if (selection === 'operand') {
+        if (operation.innerText && !operation.innerText.includes('=')) {
+            const lastValue = +operation.innerText.slice(0, -2);
+            const operand = getOperandText(operation.innerText.slice(-1));
             const result = operate(operand, lastValue, currentValue);
             operation.innerText = `${result} ${e.target.innerText}`;
         } else {
-            operation.innerText = `${+current.innerText} ${e.target.innerText}`;
+            operation.innerText = `${currentValue} ${e.target.innerText}`;
         }
 
         current.classList.add('saved');
