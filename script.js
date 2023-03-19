046640669e5f58d245fc788ddfe9c76f4cfbfc25
Tan Yi Jun <tanyijun6@gmail.com>
2023-03-20 02:29:19 +0800

    Clean up code

--- script.js
+++ script.js
@@ -77,44 +77,69 @@ function populateDisplay(e) {
     const selection = e.target.dataset.type;
     const current = document.querySelector('#current-value');
     const operation = document.querySelector("#operation");
+
     const currentValue = +current.innerText;
+    if (isNaN(currentValue)) {
+        current.innerText = 0;
+        operation.innerText = '';
+        return;
+    }
 
-    if (selection === 'number') {
-        if (currentValue === 0 || current.classList.contains('saved')) {
-            current.innerText = e.target.innerText;
-            current.classList.remove('saved');
-        } else {
-            current.innerText += e.target.innerText;
-        }
-    } else if (selection === 'enter') {
-        if (current.classList.contains('saved')) {
-            return;
-        }
-        if (operation.innerText) {
-            const lastValue = +operation.innerText.slice(0, -2);
-            const operand = getOperandText(operation.innerText.slice(-1));
-            const result = operate(operand, lastValue, currentValue);
-            operation.innerText = `${lastValue} ${getOperandSymbol(operand)} ${currentValue} =`;
-            current.innerText = result;
+    switch(selection) {
+        case 'number':
+            if (currentValue === 0 || current.classList.contains('saved')) {
+                current.innerText = e.target.innerText;
+                current.classList.remove('saved');
+            } else {
+                current.innerText += e.target.innerText;
+            }
+            break;
+
+        case 'enter':
+        case 'operand':
+            if (operation.innerText) {
+                const lastValue = +operation.innerText.slice(0, -2);
+                const operand = getOperandText(operation.innerText.slice(-1));
+
+                if (currentValue === 0 && operand === 'divide') {
+                    current.innerText = 'ERR!';
+                    return;
+                }
+                
+                const result = operate(operand, lastValue, currentValue);
+
+                if (selection === 'enter') {
+                    if (current.classList.contains('saved')) {
+                        return;
+                    }
+                    operation.innerText = `${lastValue} ${getOperandSymbol(operand)} ${currentValue} =`;
+                    current.innerText = result;
+                } else {
+                    if (!operation.innerText.includes('=')) {
+                        operation.innerText = `${result} ${e.target.innerText}`;
+                    } else {
+                        operation.innerText = `${currentValue} ${e.target.innerText}`;
+                    }
+                }
+            } else {
+                if (selection === 'operand') {
+                    operation.innerText = `${currentValue} ${e.target.innerText}`;
+                }
+            }
             current.classList.add('saved');
-        }
-    } else if (selection === 'operand') {
-        if (operation.innerText && !operation.innerText.includes('=')) {
-            const lastValue = +operation.innerText.slice(0, -2);
-            const operand = getOperandText(operation.innerText.slice(-1));
-            const result = operate(operand, lastValue, currentValue);
-            operation.innerText = `${result} ${e.target.innerText}`;
-        } else {
-            operation.innerText = `${currentValue} ${e.target.innerText}`;
-        }
+            break;
 
-        current.classList.add('saved');
-    } else if (selection === 'clear') {
-        cleanUp();
-    } else if (selection === 'delete') {
-        current.innerText = isSingleDigit(current.innerText) ? 0 : current.innerText.slice(0, -1);
-    } else if (selection === 'negative') {
-        current.innerText *= -1;
+        case 'clear' :
+            cleanUp();
+            break;
+
+        case 'delete' :
+            current.innerText = isSingleDigit(current.innerText) ? 0 : current.innerText.slice(0, -1);
+            break;
+
+        case 'negative':
+            current.innerText *= -1;
+            break;
     }
 }
 
