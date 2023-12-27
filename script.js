ecf6eac7b69a2418f780ed925ad02b4fee41d337
Tan Yi Jun <tanyijun6@gmail.com>
2023-12-27 16:59:20 +0800

    Add calculator operation process

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
@@ -14,7 +14,29 @@ function divide(a, b) {
     return a / b;
 }
 
-console.log(add(1,2));
-console.log(subtract(1,2));
-console.log(multiply(1,2));
-console.log(divide(1,2));
+function calculate(operation) {
+    const split = operation.split(' ');
+    const num1 = split[0];
+    const operator = split[1];
+    const num2 = split[2];
+    const result = operate(operator, num1, num2);
+    return result;
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+
+        case '-':
+            return subtract(num1, num2);
+
+        case '*':
+            return multiply(num1, num2);
+
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+console.log(calculate('1 + 2'));
